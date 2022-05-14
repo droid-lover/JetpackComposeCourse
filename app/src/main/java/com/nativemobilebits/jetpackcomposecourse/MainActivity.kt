6cb077a1b50840d507e927680ec898e7a837fbb6
droid-lover <ersachin20@gmail.com>
2022-05-14 16:21:39 +0530

    How to use custom height and width inside a composable

--- app/src/main/java/com/nativemobilebits/jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/nativemobilebits/jetpackcomposecourse/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nativemobilebits.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +27,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WelcomeText(value:String){
-    Text(text = value)
+    Text(
+        text = value,
+        modifier = Modifier
+            .width(100.dp)
+            .height(80.dp)
+    )
 }
 
 @Preview(showBackground = true)
